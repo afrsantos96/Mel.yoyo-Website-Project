969e5976cd9cff2569d5b50943cc226e7c602119
Andre Santos <afrsantos96@gmail.com>
2023-01-10 01:15:41 +0000

    gallery

--- src/views/About.js
+++ src/views/About.js
@@ -5,7 +5,7 @@ const About = () => (
     <div className='about'>
         <div className='about-row row'>
             <div className='col-4'>
-                <img src='./media/images/about/mel.png' className='about-mel-img' width='300px'></img>
+                <img src='./media/images/about/mel.png' className='about-mel-img' alt='Mel' width='300px'></img>
             </div>
             <div className='col-5 about-mel-txt'>
                 <h1>It's me Mel</h1>
@@ -18,7 +18,7 @@ const About = () => (
                 <p>Text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel</p>
             </div>
             <div className='col-4'>
-                <img src='./media/images/about/studio.jpg' className='about-mel-img' width='300px'></img>
+                <img src='./media/images/about/studio.jpg' className="about-mel-img" alt="Studio" width='300px'></img>
             </div>
         </div>
     </div>
--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React,{useEffect, useState}  from 'react';
+import './styles/Gallery.css'
 
-const Gallery = () => (
-    <div className='Gallery'>
-        <h1>Gallery</h1>
-        <p>Welcome</p>
-    </div>
-)
+const Gallery = props => {
+ 
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [gallery, setGallery] = useState([]);
+
+    useEffect(() => {
+        fetch('./data/gallery.json')
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setGallery(data);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+      }, []);
+
+      if (error) {
+        return <p>Error: {error.message}</p>;
+    } else if (!isLoaded) {
+        return <p>Loading...</p>;
+    } else {
+        return (
+            <>
+                {gallery.map( (image,index) => {
+                        return (
+                            <div key={index} className="gallery-img">
+                                <img src={image.url} alt="tattoo" />
+                                <div className="overlay">
+                                <p className="gallery-img-overlay">{image.title}</p>
+                                </div>
+                            </div>
+                    );
+                }
+            )}
+            </>
+        );
+    }
+
+    
+}
 
 export default Gallery;
