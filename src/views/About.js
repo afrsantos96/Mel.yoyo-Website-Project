c23d65eaadcef6e7e53126c816568724dd0b7617
Andre Santos <afrsantos96@gmail.com>
2023-02-02 23:21:21 +0000

    not found

--- src/views/About.js
+++ src/views/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles/About.css';
 
 
@@ -10,8 +11,10 @@ const About = () => (
             </div>
             <div className='about__mel_txt-container'>
                 <h1 className='about__title'>It's me Mel</h1>
-                <p className='about__mel__txt'>Text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel text about mel</p>
-                <p className="about__mel__quote">Text about <a href="#Gallery"className='about__link'>mel</a></p>
+                <p className='about__mel__txt'>Born in London, with a Portuguese heart.<br/> I discovered design at a young age, and since then it has progressed to a passion and profession that I am proud of.</p>
+                <p className='about__mel__txt'>I'm a tattoo artist in my spare time, I like to keep up to date and active. <br/>
+                I travel a lot and I think that gives me a good visual culture. I love ceramics, notebooks and imperfect shapes.</p>
+                <p className="about__mel__quote">Go pick your <Link to={"/shop"} className='about__link'>next tattoo.</Link></p>
                 <img src='./media/images/home/home3.png' className="about__heart" alt="red heart draw with face"></img>
 
             </div>
--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles/NotFound.css';
 
 const NotFound = () => (
-    <div className='NotFound'>
-        <h1>Oh no something went wrong!</h1>
-
+    <section className='notfound'>
+        <div className='notfound__container'>
         <Link className='nav-link' to={"/"}><i class="fa-solid fa-angle-left"></i> Go Back</Link>
-    </div>
+            <h1 className='notfound__txt'>Oh no something went wrong!</h1>
+            <div className='notfound__img__container'>
+                <img src='./media//images/boo.png' className="notfound__img" alt="ghost doodle"></img>
+            </div>
+        </div>
+    </section>
 )
 
 export default NotFound;
